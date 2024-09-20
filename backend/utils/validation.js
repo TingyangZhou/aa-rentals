94687d333cf1cf4a73bfdec58bc7aa5a0e0a3b18
Adrian-gh1 <Adrian22046@gmail.com>
2024-09-20 15:25:20 -0700

    Add User input validation on user login backend endpoint

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,5 +1,7 @@
 // backend/utils/validation.js
 const { validationResult } = require('express-validator');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
@@ -20,6 +22,55 @@ const handleValidationErrors = (req, _res, next) => {
   }
   next();
 };
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+// Log in
+router.post(
+    '/',
+    validateLogin,
+    async (req, res, next) => {
+      const { credential, password } = req.body;
+  
+      const user = await User.unscoped().findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+  
+      if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided credentials were invalid.' };
+        return next(err);
+      }
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
 
 module.exports = {
   handleValidationErrors
