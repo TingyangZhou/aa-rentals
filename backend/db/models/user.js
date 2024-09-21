9374ecf1e488efa227ec4459cfa3dbd97a396f50
Adrian-gh1 <Adrian22046@gmail.com>
2024-09-20 17:55:07 -0700

    Refactor for firstName and lastName

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -25,6 +25,20 @@ module.exports = (sequelize, DataTypes) => {
           },
         },
       },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 30],
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 30],
+        },
+      },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
--- backend/db/seeders/20240919225551-demo-user.js
+++ backend/db/seeders/20240919225551-demo-user.js
@@ -14,16 +14,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Demo-fname',
+        lastName: 'Demo-lname',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: 'fake1-fname',
+        lastName: 'fake1-lname',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: 'fake2-fname',
+        lastName: 'fake2-lname',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], { validate: true });
