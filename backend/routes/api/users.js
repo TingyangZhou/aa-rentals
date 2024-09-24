e30688c128f1464e5b9bd45b46582001461eabe6
Adrian-gh1 <Adrian22046@gmail.com>
2024-09-24 10:29:46 -0700

    Testing Successful - Sign Up a User

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,6 +7,7 @@ const { User } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { Op } = require('sequelize');
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Invalid email'),
   check('username')
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
@@ -23,6 +24,15 @@ const validateSignup = [
     .not()
     .isEmail()
     .withMessage('Username cannot be an email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Username is required'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('First Name is required'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Last Name is required'),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
@@ -34,9 +44,29 @@ const validateSignup = [
 router.post(
   '/',
   validateSignup,
-  async (req, res) => {
+  async (req, res) => {    
     const { email, password, username, firstName, lastName } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
+
+    const errors = {};
+    const existingEmail = await User.findOne({where: {email}});
+    const existingUsername = await User.findOne({where: {username}});
+
+      if (existingEmail) {
+        errors.email = 'User with that email already exists';
+      }
+      if (existingUsername) {
+        errors.username = 'User with that username already exists';
+      }
+
+      if (existingEmail || existingUsername) {
+        res.status(500);
+        return res.json({
+          message: 'User already exists',
+          errors
+        });
+      }
+
     const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
     const safeUser = {
@@ -49,6 +79,7 @@ router.post(
 
     await setTokenCookie(res, safeUser);
 
+    res.status(201);
     return res.json({
       user: safeUser
     });
