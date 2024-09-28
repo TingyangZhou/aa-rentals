3ca0b44c0556a9e647e6751df2846a347e46e69f
Tingyang Zhou <zty_312@163.com>
2024-09-28 16:14:35 -0700

    render debug psql

--- backend/db/migrations/20240924180632-create-spot.js
+++ backend/db/migrations/20240924180632-create-spot.js
@@ -69,9 +69,11 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = "Spots";
+    return queryInterface.dropTable(options);
+    
   }
 };
