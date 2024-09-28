f755dcc1cd3b4334e36c06413551092ce140a188
Adrian-gh1 <Adrian22046@gmail.com>
2024-09-28 14:10:09 -0700

    debug - Users/PostgreSQL Error

--- backend/db/migrations/20240924180632-create-spot.js
+++ backend/db/migrations/20240924180632-create-spot.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references:{
-          model:'Users'
+          model:'users'
         },
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE'
