b4f6d291e7108af36e4a7052b70b9050828ee321
Adrian-gh1 <Adrian22046@gmail.com>
2024-09-28 02:34:49 -0700

    npm test - Delete a Spot Image

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -8,7 +8,7 @@ const checkSpotImageExists = async (req, res, next) =>{
     
         let spotImage = await SpotImage.findByPk(spotImageId);
         if (!spotImage) {
-            const err = new Error(" Spot Image couldn't be found");
+            const err = new Error("Spot Image couldn't be found");
             err.status = 404;
             return next(err);
         }  
@@ -35,7 +35,7 @@ const checkSpotImageExists = async (req, res, next) =>{
      err.status = 403;
      return next(err);
    }
-   res.status(200).json("Successfully deleted")
+   res.status(200).json({message: "Successfully deleted"})
    
  })
  
