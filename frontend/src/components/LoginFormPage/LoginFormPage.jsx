691bfe3d0b77e72778a5e4d6cefff6066854ad2d
Tingyang Zhou <zty_312@163.com>
2024-10-12 19:42:15 -0700

    LoginFormPage component

---
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -0,0 +1,77 @@
+// frontend/src/components/LoginFormPage/LoginFormPage.jsx
+
+import { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import './LoginForm.css';
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  if (sessionUser) return <Navigate to="/" replace={true} />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    let errorObj = {};
+    if (!credential.trim()) {
+        errorObj.credential = 'Username or email cannot be empty';
+    }
+    
+    if (!password.trimEnd()) {
+        errorObj.password = 'Password cannot be empty';
+    }
+
+    setErrors(errorObj);
+    if(Object.keys(errorObj).length !==0 ) {
+        setErrors(errorObj);
+        return;
+    }
+    
+    
+    return dispatch(sessionActions.login({ credential, password })).catch(
+        async (res) => {
+          const data = await res.json();
+          if (data?.errors) setErrors(data.errors);
+        }
+      );
+  };
+
+
+  return (
+    <>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+
+          />
+        </label>
+        {errors.credential && <p style={{ color: 'red', fontStyle: 'italic' }}>{errors.credential}</p>}
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+
+          />
+        </label>
+        {errors.password && <p style={{ color: 'red', fontStyle: 'italic' }}>{errors.password}</p>}
+        {errors && <p>{errors.message}</p>}
+        <button type="submit">Log In</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginFormPage;
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,60 +1,57 @@
-//frontend/src/store/session.js
-import { csrfFetch } from './csrf'
+// frontend/src/store/session.js
+
+import { csrfFetch } from './csrf';
 
 /** Action Type Constants: */
+
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
 
 /**  Action Creators: */
+
 const setUser = (user) => {
-    return {
-      type: SET_USER,
-      payload: user
-    };
+  return {
+    type: SET_USER,
+    payload: user
   };
+};
 
 const removeUser = () => {
-    return {
-      type: REMOVE_USER
-    };
+  return {
+    type: REMOVE_USER
   };
-
+};
 
 /** Thunk Action Creators: */
-// Login
-export const login = (user) => async(dispatch) => {
-    const { credential, password } = user;
-    const res = await csrfFetch(
-        '/api/session',
-        {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                credential,
-                password
-            })
-        }
-    ); //end of csrfFetch
-  
-      const data = await res.json();
-    dispatch(setUser(data.user));
-    return res;
-  
-}
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      credential,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
 
-// Reducer
+//Reducer
+const initialState = { user: null };
 
-const sessionReducer = (state = {user: null}, action) => {
-    switch (action.type) {
-        case SET_USER: 
-            return {...state, user: action.payload};
-        case REMOVE_USER:
-            return {...state, user: null};
-        default: 
-            return state;
-    }
-}
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.payload };
+    case REMOVE_USER:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
 
 export default sessionReducer;
